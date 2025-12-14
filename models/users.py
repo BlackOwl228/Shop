659cd5344097faa8c8b994a379a879cae8632e29
BlackOwl <petr228338999666@gmail.com>
2025-12-14 22:14:06 +0400

    Новый проект, фиксирую структуру проекта

---
+++ models/users.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from core.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    hash: Mapped[str] = mapped_column(String(255), nullable=False)
